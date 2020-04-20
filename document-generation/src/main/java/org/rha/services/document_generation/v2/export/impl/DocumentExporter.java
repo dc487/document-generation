8ebeb4e1089f1d172a6672c0382d132157b42e1e
SamUK93 <sammyryan93@hotmail.co.uk>
2020-04-20 16:15:53 +0100

    - (WIP) add input stream to savedocument method params

--- document-generation/src/main/java/org/rha/services/document_generation/v2/export/impl/DocumentExporter.java
+++ document-generation/src/main/java/org/rha/services/document_generation/v2/export/impl/DocumentExporter.java
@@ -9,6 +9,9 @@ import org.rha.services.document_generation.v2.export.dto.ExportSystem;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
 import java.net.URI;
 
 @ApplicationScoped
@@ -17,11 +20,20 @@ public class DocumentExporter implements IDocumentExporter {
     @Named("sharepoint-store")
     IDocumentStore sharePointStore;
 
+    @Inject
+    Client httpRequestClient;
+
     @Override
     public URI saveDocument(ExportDocumentRequestMessage requestMessage) throws Exception {
+
+        final InputStream documentContent = httpRequestClient.target(requestMessage.getDocumentUri())
+                .request()
+                .accept(MediaType.APPLICATION_OCTET_STREAM_TYPE)
+                .get(InputStream.class);
+
         switch (requestMessage.getExportSystemId()) {
             case ExportSystem.SHAREPOINT:
-                return this.sharePointStore.saveDocument(requestMessage.getExportMetadata());
+                return this.sharePointStore.saveDocument(documentContent, requestMessage.getExportMetadata());
             default:
                 throw new DocumentExportException("Unsupported export system");
         }
--- document-generation/src/main/java/org/rha/services/document_generation/v2/export/impl/SharePointStore.java
+++ document-generation/src/main/java/org/rha/services/document_generation/v2/export/impl/SharePointStore.java
@@ -4,14 +4,22 @@ import org.rha.services.document_generation.v2.export.IDocumentStore;
 import org.rha.services.document_generation.v2.export.dto.ExportMetadata;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.client.Client;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.UUID;
 
 @Named("sharepoint-store")
 @ApplicationScoped
 public class SharePointStore implements IDocumentStore {
+    @Inject
+    Client httpRequestClient;
+
     @Override
-    public URI saveDocument(ExportMetadata exportData) {
-        return null;
+    public URI saveDocument(InputStream documentContent, ExportMetadata exportData) {
+        //TODO: This will make the call to the sharepoint service to upload the new document, and then return the URI.
+        return URI.create("http://sharepointurl.com/document/" + UUID.randomUUID().toString());
     }
 }
