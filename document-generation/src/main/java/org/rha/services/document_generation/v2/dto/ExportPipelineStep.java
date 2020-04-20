6d3b421e63d6e1e515b8f5a2f117efde4b92d975
Dave C <dc487@github.com>
2020-04-20 18:37:45 +0100

    Add interface definitions for the document processing pipeline/choreography

---
+++ document-generation/src/main/java/org/rha/services/document_generation/v2/dto/ExportPipelineStep.java
@@ -0,0 +1,30 @@
+package org.rha.services.document_generation.v2.dto;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+public class ExportPipelineStep extends PipelineStep {
+    private String exportSystemId;
+    private Map<String, String> exportMetadata;
+
+    public ExportPipelineStep() {
+        super(PipelineStepType.EXPORT);
+    }
+
+    public String getExportSystemId() {
+        return exportSystemId;
+    }
+
+    public void setExportSystemId(final String exportSystemId) {
+        this.exportSystemId = exportSystemId;
+    }
+
+    public Map<String, String> getExportMetadata() {
+        return exportMetadata;
+    }
+
+    public void setExportMetadata(final Map<String, String> exportMetadata) {
+        this.exportMetadata = Objects.requireNonNullElse(exportMetadata, Collections.emptyMap());
+    }
+}
