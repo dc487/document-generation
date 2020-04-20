392c39a3718762324adaa61bfae922dcc136f334
SamUK93 <sammyryan93@hotmail.co.uk>
2020-04-20 16:15:53 +0100

    - (WIP) add input stream to savedocument method params

--- src/main/java/org/rha/services/document_generation/v2/export/IDocumentStore.java
+++ src/main/java/org/rha/services/document_generation/v2/export/IDocumentStore.java
@@ -2,8 +2,9 @@ package org.rha.services.document_generation.v2.export;
 
 import org.rha.services.document_generation.v2.export.dto.ExportMetadata;
 
+import java.io.InputStream;
 import java.net.URI;
 
 public interface IDocumentStore {
-    URI saveDocument(ExportMetadata exportData);
+    URI saveDocument(InputStream documentContent, ExportMetadata exportData);
 }
