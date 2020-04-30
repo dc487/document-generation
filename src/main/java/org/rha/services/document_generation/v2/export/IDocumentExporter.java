c53b1dae5968f78a3427b1148090cba87a559845
SamUK93 <sammyryan93@hotmail.co.uk>
2020-04-30 15:04:30 +0100

    Parent POM cleanup and move some dependencies to conversion POM

--- src/main/java/org/rha/services/document_generation/v2/export/IDocumentExporter.java
+++
@@ -1,9 +0,0 @@
-package org.rha.services.document_generation.v2.export;
-
-import org.rha.services.document_generation.v2.export.dto.ExportDocumentRequestMessage;
-
-import java.net.URI;
-
-public interface IDocumentExporter {
-    URI saveDocument(ExportDocumentRequestMessage requestMessage) throws Exception;
-}
--- src/main/java/org/rha/services/document_generation/v2/export/dto/ExportSystem.java
+++
@@ -1,6 +0,0 @@
-package org.rha.services.document_generation.v2.export.dto;
-
-public final class ExportSystem {
-    public static final String SHAREPOINT = "SHAREPOINT";
-    public static final String FILE = "FILE";
-}
