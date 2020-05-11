ab276a91f27a0f0aa481e69e2db9983e31585316
SamUK93 <sammyryan93@hotmail.co.uk>
2020-05-11 09:32:59 +0100

    - Export cleanup

--- conversion-service/src/main/java/org/rha/services/document_generation/conversion/core/model/DocumentOutputFormat.java
+++
@@ -1,6 +0,0 @@
-package org.rha.services.document_generation.conversion.core.model;
-
-public enum DocumentOutputFormat {
-    DOCX,
-    PDF
-}
--- shared-libs/src/main/java/export/ExportDocumentRequestMessage.java
+++ shared-libs/src/main/java/export/ExportDocumentRequestMessage.java
@@ -4,7 +4,7 @@ import java.net.URI;
 import java.util.Map;
 
 public class ExportDocumentRequestMessage {
-    private URI documentUri;
+    private URI documentContentUri;
     private String exportSystemId;
     private Map<String,String> exportMetadata;
     private String sourceSystemId;
@@ -15,12 +15,12 @@ public class ExportDocumentRequestMessage {
 
     }
 
-    public URI getDocumentUri() {
-        return documentUri;
+    public URI getDocumentContentUri() {
+        return documentContentUri;
     }
 
-    public void setDocumentUri(URI documentUri) {
-        this.documentUri = documentUri;
+    public void setDocumentContentUri(URI documentContentUri) {
+        this.documentContentUri = documentContentUri;
     }
 
     public String getExportSystemId() {
